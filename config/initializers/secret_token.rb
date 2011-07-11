d85d30de6a08d08decf782444b4661dbcda533bc
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-07-11 16:08:49 +0300

    refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+F1Refinery::Application.config.secret_token = '6109558696dfb724694b4cef976a89819fc3f8f71fb5d8ce6afe15286e7c66c4948975a686a3309b59e819e8b0a42f125ba13dd2a5e967a5e00c464f1fc58560'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+F1Refinery::Application.config.session_store :cookie_store, :key => '_f1-refinery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# F1Refinery::Application.config.session_store :active_record_store
