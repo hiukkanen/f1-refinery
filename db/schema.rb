fd438a836e292735433f5a2197987ce2afd97a91
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-09 20:53:25 +0300

    Install news engine and run generation scripts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110711130775) do
+ActiveRecord::Schema.define(:version => 20110809175235) do
 
   create_table "images", :force => true do |t|
     t.string   "image_mime_type"
@@ -23,6 +23,30 @@ ActiveRecord::Schema.define(:version => 20110711130775) do
     t.string   "image_uid"
     t.string   "image_ext"
   end
+
+  create_table "news_item_translations", :force => true do |t|
+    t.integer  "news_item_id"
+    t.string   "locale"
+    t.text     "body"
+    t.string   "title"
+    t.string   "external_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "news_item_translations", ["news_item_id"], :name => "index_news_item_translations_on_news_item_id"
+
+  create_table "news_items", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "publish_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "image_id"
+    t.datetime "expiration_date"
+  end
+
+  add_index "news_items", ["id"], :name => "index_news_items_on_id"
 
   create_table "page_part_translations", :force => true do |t|
     t.integer  "page_part_id"
