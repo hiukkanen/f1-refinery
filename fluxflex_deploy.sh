e048099a69258ba7758096555598d8c18be71730
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-09 20:57:04 +0300

    Always install bundler

--- fluxflex_deploy.sh
+++ fluxflex_deploy.sh
@@ -2,6 +2,7 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1/
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
+gem install bundler
 bundle install
 
 rake db:migrate RAILS_ENV=production
